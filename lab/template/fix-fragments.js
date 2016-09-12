91bd9b402df180402301a6d848f926ee553785b6
Maxwell Terpstra <telic.detour+github@gmail.com>
2016-09-12 12:35:37 -0700

    use strict mode

--- lab/template/fix-fragments.js
+++ lab/template/fix-fragments.js
@@ -1,4 +1,5 @@
 (function() {
+	"use strict";
 	var inPage = document.querySelectorAll("a[href^='#']");
 	for (var i=inPage.length-1; i>=0; i--) {
 		inPage[i].href = window.location + inPage[i].getAttribute('href');
