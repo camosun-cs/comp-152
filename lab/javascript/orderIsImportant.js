686e065244fe4cab6ce67c1e8ae99b26b4ec0881
Maxwell Terpstra <telic.detour+github@gmail.com>
2016-11-01 20:35:22 -0700

    highlight the last element before the script

--- lab/javascript/orderIsImportant.js
+++ lab/javascript/orderIsImportant.js
@@ -1,1 +1,3 @@
-alert("There are currently " + document.querySelectorAll("*").length + " elements on the page");
+var elements = document.querySelectorAll("*");
+alert("There are currently " + elements.length + " elements on the page");
+elements[elements.length - 2].style.backgroundColor = "red";
